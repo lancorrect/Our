600854fbe6b426f4c8e7f0f7cbd200dd81c53fb0
lancorrect <1187671740@qq.com>
2022-12-21 20:55:55 +0800

    20221221

--- find_best_result.py
+++ find_best_result.py
@@ -6,9 +6,10 @@ best_f1 = 0
 best_agrs = None
 best_model_path =None
 best_path = None
-for path in os.listdir('./results'):
+base_dir = './results_bert/'
+for path in os.listdir(base_dir):
     flag = True
-    with open(os.path.join('./results/' + path), 'r') as f:
+    with open(os.path.join(base_dir + path), 'r') as f:
         results = eval(f.readline())
         tmp_acc = results['acc']
         tmp_f1 = results['f1']
@@ -17,23 +18,24 @@ for path in os.listdir('./results'):
             best_f1 = tmp_f1
             best_model_path = results['model_path']
             best_path = str(path)
+            best_agrs = results
         f.close()
 
 print('best_acc:%f,' % (best_acc))
 print('best_f1:%f,' % (best_f1))
 print('model_path:%s' % (best_model_path))
 print('best_dirpath:%s' % (best_path))
-for key in results.keys():
+for key in best_agrs.keys():
     if key in ['acc', 'f1', 'model_path']:
         continue
     else:
-        print('%s:%s' % (key, str(results[key])), end=' ')
+        print('%s:%s' % (key, str(best_agrs[key])), end=' ')
 
 print('\n'+'-'*60)
 print('start copy')
 destpath = './best_result_log'
 if not os.path.exists(destpath):
     os.mkdir(destpath, mode=0o777)
-shutil.copy(os.path.join('./results', best_path), os.path.join(destpath, best_path))
+shutil.copy(os.path.join(base_dir, best_path), os.path.join(destpath, best_path))
 print('copy complete')
 print('-'*60)
